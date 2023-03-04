97e0cfadd8995d7ca91cd7d90d2e999898870b1b
antoinepolster <20090@ecam.be>
2023-03-04 16:17:46 +0100

    Premier commit

---
+++ files_labo_1/test_utils.py
@@ -0,0 +1,26 @@
+# test_utils.py
+# Author: Sébastien Combéfis
+# Version: February 8, 2018
+import unittest
+import utils
+import pytest
+import math
+
+def test_fact():
+    assert utils.fact(0) == 1 
+    assert utils.fact(4) == 24
+    with pytest.raises(ValueError):
+        utils.fact(-1)
+    
+def test_roots():
+    assert isinstance(utils.roots(2, 0, 0), tuple)
+    assert utils.roots(2, 0 ,0) == pytest.approx((0.0,))
+    assert utils.roots(2, 0 ,-2) == pytest.approx((-1.0, 1.0))
+    
+def test_integrate():
+    assert utils.integrate('x ** 2 - 1', -1, 1) == -1.3333333333333335
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
+    runner = unittest.TextTestRunner()
+    exit(not runner.run(suite).wasSuccessful())
---
+++ files_labo_1/utils.py
@@ -0,0 +1,60 @@
+# utils.py
+# Math library
+# Author: Sébastien Combéfis
+# Version: February 8, 2018
+from cmath import sqrt
+from math import factorial
+import math
+import scipy
+import scipy.integrate as integrale
+import scipy.special as special
+
+def fact(n):
+
+	"""Computes the factorial of a natural number.
+	
+	Pre: -
+	Post: Returns the factorial of 'n'.
+	Throws: ValueError if n < 0
+	"""
+	if n <0:
+		raise ValueError("n must be positive")
+	res = 1 
+	for i in range (1, n+1):
+		res *= i
+	return res
+
+def roots(a, b, c):
+	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
+	
+	Pre: -
+	Post: Returns a tuple with zero, one or two elements corresponding
+		to the roots of the ax^2 + bx + c polynomial.
+	"""
+	delta = b**2 - 4*a*c
+	if delta < 0:
+		return tuple()
+	if delta == 0:
+		return(-b/2/a,)
+	sqrtDelta = math.sqrt(delta)
+	return ((-b - sqrtDelta)/(2*a), (-b + sqrtDelta)/(2*a))
+	
+def integrate(function, lower, upper):
+	"""Approximates the integral of a fonction between two bounds
+	
+	Pre: 'function' is a valid Python expression with x as a variable,
+		'lower' <= 'upper',
+		'function' continuous and integrable between 'lower‘ and 'upper'.
+	Post: Returns an approximation of the integral from 'lower' to 'upper'
+		of the specified 'function'.
+
+	Hint: You can use the 'integrate' function of the module 'scipy' and
+		you'll probably need the 'eval' function to evaluate the function
+		to integrate given as a string.
+	"""
+	result = integrale.quad(lambda x: eval(function, {"x": x}), lower, upper)[0]
+	return result
+if __name__ == '__main__':
+	print(fact(4))
+	print(roots(1, 0, 1))
+	print(integrate('x ** 2 - 1', -1, 1))
